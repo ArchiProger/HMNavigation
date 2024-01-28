401cdd5d18c92c4cf33f63191b1961319024e28b
Arthur <archiworking.d@gmail.com>
2024-01-28 18:02:05 +0300

    Documentation

--- Sources/NavigationSheet/View/SheetView.swift
+++ Sources/NavigationSheet/View/SheetView.swift
@@ -20,12 +20,17 @@ public struct SheetView<Content: View, Modifier: ViewModifier>: View {
     }
             
     // MARK: - Sheet modifiers
+    
+    /// Specifies the display style of the modal window. On top of navigation or by default
+    /// - Parameter type: Modal window style
     public func sheetDisplayType(type: SheetDisplayType) -> Self {
         self.sheetModel.type = type
         
         return self
     }
     
+    /// Allowable height of the modal window
+    /// - Parameter detents: Array of permissible heights
     public func sheetDetents(detents: [UISheetPresentationController.Detent]) -> Self {
         self.sheetModel.presentationPreferences.append {
             $0?.detents = detents
@@ -33,7 +38,9 @@ public struct SheetView<Content: View, Modifier: ViewModifier>: View {
         
         return self
     }
-        
+    
+    /// Turning on/off the drag indicator
+    /// - Parameter visible: Drag indicator display
     public func sheetPrefersGrabberVisible(visible: Bool) -> Self {
         self.sheetModel.presentationPreferences.append {
             $0?.prefersGrabberVisible = visible
@@ -41,7 +48,9 @@ public struct SheetView<Content: View, Modifier: ViewModifier>: View {
         
         return self
     }
-        
+    
+    /// Round the corners of the modal window
+    /// - Parameter radius: Radius of rounding
     public func sheetPreferredCornerRadius(radius: CGFloat) -> Self {
         self.sheetModel.presentationPreferences.append {
             $0?.preferredCornerRadius = radius
@@ -49,25 +58,34 @@ public struct SheetView<Content: View, Modifier: ViewModifier>: View {
         
         return self
     }
-    
+        
+    /// Background style of the modal window
+    /// - Parameter style: Background style
     public func sheetBackground<S: ShapeStyle>(_ style: S) -> Self {
         self.sheetModel.backgroundColor = AnyShapeStyle(style)
         
         return self
     }
     
-    public func sheetBackground<T: View>(@ViewBuilder content: () -> T) -> Self {        
+    /// Background View
+    /// - Parameter content: background View
+    public func sheetBackground<T: View>(@ViewBuilder content: () -> T) -> Self {
         self.sheetModel.backgroundContent = AnyView(content())
         
         return self
     }
     
+    /// Enable/disable hiding the modal window with a gesture
+    /// - Parameter status: Gesture status
     public func sheetDismissAction(_ status: SheetDismissActionStatus) -> Self {
         self.sheetModel.dismissActionStatus = status
         
         return self
     }
     
+    /// The largest detent that doesn’t dim the view underneath the sheet.
+    /// The default value is nil, which means the system adds a noninteractive dimming view underneath the sheet at all detents. Set this property to only add the dimming view at detents larger than the detent you specify. For example, set this property to medium to add the dimming view at the large detent.
+    /// Without a dimming view, the undimmed area around the sheet responds to user interaction, allowing for a nonmodal experience. You can use this behavior for sheets with interactive content underneath them.
     public func sheetBackgroundInteraction(_ identifier: UISheetPresentationController.Detent.Identifier?) -> Self {
         self.sheetModel.presentationPreferences.append { controller in
             controller?.largestUndimmedDetentIdentifier = identifier
